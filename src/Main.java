9e79eb65338ebf89343780dc0ac5b34ce6317565
Jacobo Garcia <juanjacobogarcia4@gmail.com>
2021-02-23 16:12:46 -0500

    1.0

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String line;
+
+        String message = "";
+        for (int i = 0; i < n; i++) {
+            line = br.readLine();
+
+            message += sort(line) + "\n";
+
+        }
+        System.out.println(message);
+
+        br.close();
+
+    }
+
+    public static String sort(String line) {
+        ArrayList<Integer> counters = new ArrayList<Integer>();
+        String[] input = line.split(" ");
+        double[] numbers = new double[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            numbers[i] = Double.parseDouble(input[i]);
+        }
+
+        bubbleSort(numbers, counters);
+        // System.out.println(Arrays.toString(numbers));
+        // System.out.println(counters.toString());
+
+        double prom = 0;
+
+        for (int counter : counters) {
+            prom += counter;
+        }
+        prom = prom / counters.size();
+
+
+        //System.out.println(prom);
+
+        BigDecimal formatNumber = new BigDecimal(prom);
+        formatNumber = formatNumber.setScale(2, RoundingMode.DOWN);
+
+        BigDecimal oneDecimal = new BigDecimal(formatNumber.doubleValue());
+        oneDecimal = oneDecimal.setScale(1, RoundingMode.HALF_UP);
+
+        String message;
+
+        if ((formatNumber.doubleValue() * 10) % 1 == 0) {
+            message = oneDecimal + "-";
+        } else {
+            message = formatNumber + "-";
+
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            message += numbers[i] + " ";
+
+        }
+
+        return message;
+    }
+
+    public static void bubbleSort(double[] arraySort, ArrayList<Integer> counters) {
+
+        for (int j = 1; j < arraySort.length; j++) {
+            int count = 0;
+            for (int i = 0; i < arraySort.length - j; i++) {
+                if (arraySort[i] > arraySort[i + 1]) {
+                    double temp = arraySort[i];
+                    arraySort[i] = arraySort[i + 1];
+                    arraySort[i + 1] = temp;
+                    count++;
+                }
+
+            }
+
+            counters.add(count);
+        }
+
+    }
+
+}
